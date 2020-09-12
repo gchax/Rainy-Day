6e5b754747c2da6dd75b5b1160496950699bc81b
Chanon <pcg.mega@gmail.com>
2020-09-12 11:11:35 +0700

    Add project files.

---
+++ Rhombus/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num,row,outcld,incld,rstar,rspace,i=1;
+	printf("\"When one\'s heart is broken, one shall seek for the rain\"\nSo, how much rain do you need? ");
+	scanf_s("%d",&num);
+	
+	for (row = 1;row <= num / i;row++) {
+		for (outcld = 1;outcld <= (num /2)-2 ;outcld++) printf("  ");
+		printf("( ");
+		for (incld = 1;incld <= num;incld++) printf("  ");
+		printf(" )");
+		printf("\n");
+		i++;
+	}
+	for (row = 1;row <= num;row++) {
+		for (rspace = 1;rspace <= num-row; rspace++) printf(" ");
+		for (rstar = 1;rstar <= num; rstar++) printf("/ ");
+		printf("\n");
+	}
+	printf(";)");
+
+	return 0;
+}
